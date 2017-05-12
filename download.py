7a0a0f0dd583827678fcac8a52e66008300df15a
James B <jsoftwareengineering@users.noreply.github.com>
2017-05-12 19:28:25 +0100

    update if statement to sane method

--- download.py
+++ download.py
@@ -4,18 +4,20 @@ counter = 0
 fileID = ''
 with open('links.txt') as f:
 	for line in f:
-		if len(line) < 70:
+		if 'open?id=' in line:
 			stri = line.split('id=')
 			stri = stri[1].split('\n')
 			print(str(counter) + ' ' + stri[0])
 			fileID = stri[0]
 
-		else:
+		elif 'file/d/' in line:
 			stri = line.split('/d/')
 			stri = stri[1].split('view')
 			stri = stri[0].split('/')
 			print(str(counter) + ' ' + stri[0])
 			fileID = stri[0]
+		else: 
+			print('invalid link')
 
 		link = 'http://drive.google.com/uc?export=download&id=' + fileID
 		path = 'photos/' + str(counter) + '.jpg'
